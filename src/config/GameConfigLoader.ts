750c958abcfbb135402b2d82ad3267f45e6bc9a1
Neso <Neso@users.noreply.github.com>
2025-12-01 22:12:34 +0000

    deploy: 646f1d4f681ae2ddf53a01f969a26017b600056b

--- src/config/GameConfigLoader.ts
+++
@@ -1,8 +0,0 @@
-import defaultConfig from './config.json';
-import { GameConfig } from '@core/types';
-
-export class GameConfigLoader {
-  public static load(): GameConfig {
-    return defaultConfig as GameConfig;
-  }
-}
