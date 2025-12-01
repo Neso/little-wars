31fa6bb4cd2407e3ad3856dc1d429faa2bd44a7f
Neso <Neso@users.noreply.github.com>
2025-12-01 22:00:21 +0000

    Updated graphic.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-  base: './',
+  base: '/little-wars/',
   resolve: {
     alias: {
       '@core': path.resolve(__dirname, 'src/core'),
